be4df3744fda963e4fc980f4f7b1c9354a7a48b2
Kg-Angry <alexva02@gmail.com>
2020-04-29 22:09:32 +0200

    Home page and icon

--- PMA/app/src/main/java/com/ftn/pma/MainActivity.java
+++ PMA/app/src/main/java/com/ftn/pma/MainActivity.java
@@ -3,12 +3,33 @@ package com.ftn.pma;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView logo;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //animacije za bootup
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        logo = findViewById(R.id.logo);
+        text = findViewById(R.id.textView2);
+
+        logo.setAnimation(topAnim);
+        text.setAnimation(bottomAnim);
+
     }
 }
