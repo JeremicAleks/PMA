e39847a2cef3fe7eba492f653f21b3afe59fd3dc
JeremicAleks <jeremicaleksandar96@gmail.com>
2020-04-30 00:03:43 +0200

    Login and Registration Activity

--- PMA/app/src/main/java/com/ftn/pma/view/LoginActivity.java
+++ PMA/app/src/main/java/com/ftn/pma/view/LoginActivity.java
@@ -4,39 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ftn.pma.R;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final Button login = findViewById(R.id.btn_login);
+        TextView tvSignUp = findViewById(R.id.tv_signUp);
+        final EditText etEmail = findViewById(R.id.et_email);
+        final EditText etPassword = findViewById(R.id.et_password);
 
-        login.setOnClickListener(new View.OnClickListener() {
+        Button btnLogin = findViewById(R.id.btn_login);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent);
-                finish();
-
-                TextView tvName = findViewById(R.id.tv_username);
-
-                Bundle extras = getIntent().getExtras();
-
-                if(extras.containsKey("User")){
-                    tvName.setText("AAAAAAA");
-                }
-
+                String email = etEmail.getText().toString();
+                String pass = etPassword.getText().toString();
+                if(email.equals("admin") && pass.equals("admin")) {
+                    Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+            }else{
+                Toast.makeText(LoginActivity.this,"Failed Login",Toast.LENGTH_SHORT).show();
+            }
 
             }
         });
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                    startActivity(intent);
+            }
+        });
+
 
     }
 }
