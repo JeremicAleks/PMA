07bce2eabc28ffea9801fb0ef9f69d8776f2dc98
Kg-Angry <alexva02@gmail.com>
2020-05-01 00:39:44 +0200

    Uradjen About i polozena varijanta

--- PMA/app/src/main/java/com/ftn/pma/view/AboutActivity.java
+++ PMA/app/src/main/java/com/ftn/pma/view/AboutActivity.java
@@ -1,41 +1,70 @@
 package com.ftn.pma.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.ftn.pma.R;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class AboutActivity extends FragmentActivity implements OnMapReadyCallback {
+public class AboutActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap maps;
     private SupportMapFragment mapFragment;
+    private EditText name_service;
+    private EditText email_service;
+    private EditText telefone_service;
+    private ImageView img_email;
+    private ImageView img_telephone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Toolbar toolbar = findViewById(R.id.toolbar_about);
+        toolbar.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        setSupportActionBar(toolbar);
 
-//        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+
+        name_service = findViewById(R.id.pt_name_service);
+        email_service = findViewById(R.id.pt_email_service);
+        telefone_service = findViewById(R.id.pt_telefone_service);
+
+        name_service.setFocusable(false);
+        email_service.setFocusable(false);
+        telefone_service.setFocusable(false);
+
+        img_email = findViewById(R.id.img_email);
+        img_telephone = findViewById(R.id.img_telephone);
+        int color = Color.parseColor("#000000");
+        img_email.setColorFilter(color);
+        img_telephone.setColorFilter(color);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-//        maps = googleMap;
-//
-//        LatLng fax_NS = new LatLng(45.245557, 19.851231);
-//        maps.addMarker(new MarkerOptions().position(fax_NS).title("Car Repair Service"));
-//
-//        maps.moveCamera(CameraUpdateFactory.newLatLng(fax_NS));
+        LatLng fax_NS = new LatLng(45.245557, 19.851231);
+        googleMap.addMarker(new MarkerOptions().position(fax_NS).title("Car Repair Service"));
+
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(fax_NS,15),2000,null);
+
     }
 }
