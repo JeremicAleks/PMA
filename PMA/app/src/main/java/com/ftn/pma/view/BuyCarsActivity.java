8a671f9acdada9269cbbdf1fd178add495185c60
Kg-Angry <alexva02@gmail.com>
2020-05-06 00:39:35 +0200

    Podesen layout BuyCars

--- PMA/app/src/main/java/com/ftn/pma/view/BuyCarsActivity.java
+++ PMA/app/src/main/java/com/ftn/pma/view/BuyCarsActivity.java
@@ -1,18 +1,30 @@
 package com.ftn.pma.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.WindowManager;
 
 import com.ftn.pma.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class BuyCarsActivity extends AppCompatActivity {
 
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_buy_cars);
+
     }
 }
