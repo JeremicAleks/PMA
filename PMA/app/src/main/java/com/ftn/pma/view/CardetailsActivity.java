d762171b3d7d7b483267721fc975f31d32c78ca8
JeremicAleks <jeremicaleksandar96@gmail.com>
2020-05-06 23:17:11 +0200

    Update CardetailsActivity.java

--- PMA/app/src/main/java/com/ftn/pma/view/CardetailsActivity.java
+++ PMA/app/src/main/java/com/ftn/pma/view/CardetailsActivity.java
@@ -2,7 +2,9 @@ package com.ftn.pma.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TabHost;
@@ -10,6 +12,7 @@ import android.widget.TabHost;
 import com.ftn.pma.R;
 import com.leo.simplearcloader.ArcConfiguration;
 import com.leo.simplearcloader.SimpleArcDialog;
+import com.leo.simplearcloader.SimpleArcLoader;
 
 public class CardetailsActivity extends AppCompatActivity {
 
@@ -35,11 +38,22 @@ public class CardetailsActivity extends AppCompatActivity {
         btBuy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SimpleArcDialog mDialog = new SimpleArcDialog(CardetailsActivity.this);
+                final SimpleArcDialog mDialog = new SimpleArcDialog(CardetailsActivity.this);
                 ArcConfiguration configuration = new ArcConfiguration(CardetailsActivity.this);
+                configuration.setAnimationSpeed(SimpleArcLoader.SPEED_MEDIUM);
+                int[] mColors = {Color.parseColor("#0266C8")};// GREEN
                 configuration.setText("Please wait..");
+                configuration.setColors(mColors);
                 mDialog.setConfiguration(configuration);
                 mDialog.show();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mDialog.cancel();
+                    }
+                },4000);
+
             }
         });
 
