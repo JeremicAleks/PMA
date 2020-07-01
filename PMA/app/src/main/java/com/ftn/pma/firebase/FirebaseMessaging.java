a69448934a6daeffa9868ea7b01853fd47660f9d
JeremicAleks <jeremicaleksandar96@gmail.com>
2020-07-02 01:36:17 +0200

    Cloud Messaging

---
+++ PMA/app/src/main/java/com/ftn/pma/firebase/FirebaseMessaging.java
@@ -0,0 +1,69 @@
+package com.ftn.pma.firebase;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.ftn.pma.R;
+import com.ftn.pma.model.Car;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.io.IOException;
+
+class FirebaseMessaging extends FirebaseMessagingService {
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        Log.d("MyToken",s);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        if(remoteMessage.getData().size() > 0){
+            //handle the data message here
+        }
+        showNotification(1,"AAAAAAA");
+        //getting the title and the body
+        String title = remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
+    }
+
+    private void showNotification(int id, String text) {
+        NotificationCompat.Builder notification = null;
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //zbog verzije androida. Koristio sam metodu za API26 a min je 24
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel notificationChannel = new NotificationChannel("ID", "Name", importance);
+            notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+            notification = new NotificationCompat.Builder(getApplicationContext(), notificationChannel.getId());
+        } else {
+            notification = new NotificationCompat.Builder(getApplicationContext());
+        }
+
+        notification.setContentTitle("New Cars");
+        notification.setLights(Color.YELLOW,2000,2000);
+        notification.setContentText("Stigao je auto "+ text);
+        notification.setTicker("New Message Alert!");
+        notification.setSmallIcon(R.drawable.ic_person_black_24dp);
+
+//        if(id == 1)
+//            notification.setSound(alarmSound);
+//        else {
+//            Vibrator v = (Vibrator) this.getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+//            // Vibrate for 500 milliseconds
+//            v.vibrate(500);
+//
+//        }
+        notificationManager.notify(111,notification.build());
+    }
+}
